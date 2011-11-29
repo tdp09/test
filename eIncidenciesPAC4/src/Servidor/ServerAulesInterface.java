c915ff34727e056fb31bba252e5c7c951f23c741
josepav <ebls123@gmail.com>
2011-11-29 22:42:16 +0100

    Test RMI

---
+++ eIncidenciesPAC4/src/Servidor/ServerAulesInterface.java
@@ -0,0 +1,9 @@
+package Servidor;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServerAulesInterface extends Remote{
+	// Metòdes comuns 
+	
+}
---
+++ eIncidenciesPAC4/src/Servidor/ServerAulesInterfaceImpl.java
@@ -0,0 +1,13 @@
+package Servidor;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ServerAulesInterfaceImpl extends UnicastRemoteObject implements ServerAulesInterface, Serializable {
+	
+}
